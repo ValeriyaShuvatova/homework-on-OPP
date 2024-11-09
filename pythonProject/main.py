a43a28858a901db5cd0aacdde6319ee7de36dcd8
Valeriya Shuvatova <valeriyashuvatova.1983@gmail.com>
2024-11-09 21:48:24 +0300

    Add changes

---
+++ pythonProject/main.py
@@ -0,0 +1,93 @@
+class Student:
+
+
+    def __init__(self, name, surname, gender):
+        self.name = name
+        self.surname = surname
+        self.gender = gender
+        self.finished_courses = []
+        self.courses_in_progress = []
+        self.grades = {}
+        #self.average = 0
+
+
+    def grades(self, lecturer, course, grade):
+        if isinstance(lecturer, Lecturer) and course in self.finished_courses and course in lecturer.courses_attached:
+            if course in lecturer.grades:
+                lecturer.grades[course] += [grade]
+            else:
+                lecturer.grades[course] = [grade]
+        else:
+            return 'Ошибка'
+
+
+
+    #def average_value(self, name, course, grades):
+
+        #if isinstance() and course in self.finished_courses and course in self.courses_in_progress:
+            #avg = sum(self.grades.values()) / len(self.grades.values())
+            #self.avg += avg
+
+
+
+    def __str__(self):
+        return f'Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за домашние задания: {self.average()}\nКурсы в процессе изучения: {", ".join(self.courses_in_progress)}\nЗавершённые курсы: {self.finished_courses}'
+
+
+
+class Mentor:
+    def __init__(self, name, surname, courses_attached):
+
+        self.name = name
+        self.surname = surname
+        self.courses_attached = []
+
+
+
+class Lecturer(Mentor):
+    def __init__(self, name, surname, courses_attached):
+        super().__init__(name, surname, courses_attached)
+        self.grades = {}
+
+    def __str__(self):
+        return f'Имя: {self.name}\nФамилия: {self.surname}'
+
+class Reviewer(Mentor):
+    def __init__(selfself, name, surname, courses_attached):
+        super().__init__(name, surname, courses_attached)
+
+    def rate_hw(self, student, course, grade):
+        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
+            if course in student.grades:
+                student.grades[course] += [grade]
+            else:
+                student.grades[course] = [grade]
+        else:
+            return 'Ошибка'
+
+    def __str__(self):
+        return f'Имя: {self.name}\nФамилия: {self.surname}'
+
+
+def __average__(self):
+    total_subject_grades = 0
+    count_subject_grades = 0
+    for subject, value in self.grades.items():
+        total_subject_grades += sum(value)
+        count_subject_grades += len(value)
+    return total_subject_grades / count_subject_grades
+
+best_student = Student('Ruoy', 'Eman', 'your_gender')
+best_student.courses_in_progress += ['Python', 'Git']
+
+
+lecturer=Lecturer('Some', 'Buddy', 'Python')
+
+cool_reviewer = Reviewer('Some', 'Бадди', 'Python')
+cool_reviewer.courses_attached += ['Python']
+
+cool_reviewer.rate_hw(best_student, 'Python', 10)
+cool_reviewer.rate_hw(best_student, 'Python', 10)
+cool_reviewer.rate_hw(best_student, 'Python', 10)
+
+print(cool_reviewer)
